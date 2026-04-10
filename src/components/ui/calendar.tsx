49bab87a228a4009af4c211c9c511e87b3ade96a
Anjali Dubey <82695549+anjalix24@users.noreply.github.com>
2026-04-10 22:50:57 +0530

    fix: resolve unused import build error in calendar component

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -42,10 +42,8 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         ...classNames,
       }}
       components={{
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        // IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        // IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
